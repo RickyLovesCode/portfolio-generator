8cd58ca55680a49b4a2fc3313cc8cf6814b73e30
Ricky27 <Rickyrenteria7@gmail.com>
2022-01-25 01:11:16 -0800

    user input func

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
+const fs = require('fs');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [myname, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-}
+const generatePage = (myname, github) => {
+
+  return `  <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${myname}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html> `;
+};
+
+fs.writeFile('index.html', generatePage(myname, github), err => {
+
+  if (err) throw err;
 
 
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
+
+console.log(myname, github);
